d35172df7550ad0ffe3bf16b5ebd6135654d74c6
Code and core <codeandcore@gmail.com>
2024-12-19 20:13:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const path = require("path");
-const generateJson = require("./api/generateJson");
 const { kv } = require("@vercel/kv");
 const cache = require("./cache");
+const generateJson = require("./api/generateJson");
 
 const app = express();
 
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ limit: "500mb", extended: true }));
-
+// Middleware to parse JSON
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use("/api/generateJson", generateJson);
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Route to read JSON files from the /tmp directory
 app.get("/data/:type?/:fileName", async (req, res) => {
   const type = req.params.type;
   const fileName = req.params.fileName;
-
-  // Validate the type
   const validTypes = ["posts", "case_study", "job_listing", "pages", "instagram", "youtube", "linkedin"];
   if (type && !validTypes.includes(type)) {
     return res.status(400).json({ message: "Invalid type" });
@@ -37,12 +30,10 @@ app.get("/data/:type?/:fileName", async (req, res) => {
   }
 });
 
-// Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
