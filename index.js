7ba0e1ad82781139519eaf8dc7aa48a24c7becd2
Code and core <codeandcore@gmail.com>
2024-12-19 20:01:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
 const express = require("express");
 const path = require("path");
+const generateJson = require("./api/generateJson");
 const { kv } = require("@vercel/kv");
 const cache = require("./cache");
 
 const app = express();
 
 // Middleware to parse JSON
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-console.log("testasdasd");
+// API routes for generating JSON
+app.use("/api/generateJson", generateJson);
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route to read JSON files from the /tmp directory
 app.get("/data/:type?/:fileName", async (req, res) => {
-  const { type, fileName } = req.params;
-  let fileNameForKv;
+  const type = req.params.type;
+  const fileName = req.params.fileName;
+
+  // Validate the type
+  const validTypes = ["posts", "case_study", "job_listing", "pages", "instagram", "youtube", "linkedin"];
+  if (type && !validTypes.includes(type)) {
+    return res.status(400).json({ message: "Invalid type" });
+  }
+  const cachedData = cache.get(fileName);
+  if (cachedData) {
+    return res.send(cachedData);
+  }
   try {
-    if (type === "options") {
-      fileNameForKv = "options";
-    } else {
-      fileNameForKv = `${type}-${fileName}`;
-    }
-    console.log("fileNameForKv",fileNameForKv);
-
-    const datajson = await kv.get(fileNameForKv);
-    console.log("datajson",datajson);
-    if (!datajson) {
-      return res.status(404).json({ message: "Data not found." });
-    }
-
-    cache.set(fileNameForKv, datajson);
-
+    const datajson = await kv.get(fileName);
+    cache.set(fileName, datajson);
     res.send(datajson);
   } catch (error) {
-    console.error("Error fetching data:", error);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
+// Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
